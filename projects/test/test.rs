7f8319522f85443c98837593336865a1f55bed9d
jayadeep217 <gaddamjayadeep@gmail.com>
2025-03-01 11:05:13 +0530

    Numeric ops

---
+++ projects/test/test.rs
@@ -0,0 +1,25 @@
+fn main() {
+    //add
+    let add = 5.0 + 1.10;
+    println!("{}", add);
+
+    //sub
+    let diff = 12 - 98;
+    println!("{}", diff);
+
+    //mull
+    let mul: u128 = 12342124 * 1232134124124;
+    println!("{}", mul);
+
+    //div
+    let div = 56/2;
+    let div_1 = 5.6/2.5;    
+    println!("{}\n{}", div, div_1);
+
+    let truncated = -5/3;
+    println!("{}", truncated);
+
+    //remainder 
+    let rem = -5%3;
+    println!("{}", rem);
+}
